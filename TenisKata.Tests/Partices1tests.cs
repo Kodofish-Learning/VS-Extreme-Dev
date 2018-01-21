01fed68729cdbb6187dd5582ad5c1fa1f7bc22c9
Kodofish Chang <kodofish.chang@johnsontechinc.com>
2018-01-21 09:34:46 +0800

    initial project

---
+++ TenisKata.Tests/Partices1tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TenisKata.Tests
+{
+    [TestFixture]
+    public class Partices1tests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
